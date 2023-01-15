36388f2bd2315727ed9d0494dfab7865fc89678d
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2023-01-15 17:03:49 +0100

    add impl. for customer endpoints

--- backend/rest_api/server.js
+++ backend/rest_api/server.js
@@ -16,43 +16,189 @@ app.use(bodyParser.json()); // support json encoded bodies
 // ################
 
 // retrieve a list of all movies available
-app.get("/movies", (req, res) => {
-    // TODO
+const selectAllMoviesText =
+    `
+    SELECT movie_id, name, description, duration, age
+    FROM movies;
+    `;
+
+app.get('/movies', (req, res) => {
+    pool.query(selectAllMoviesText)
+        .then(value => {
+            res.status(200).json(value.rows);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // retrieve details for a movie
+const selectMovieDetailsText =
+    `
+    SELECT name, description, duration, age
+    FROM movies
+    WHERE movies.movie_id = $1;
+    `;
+
 app.get("/movies/details/:movieID", (req, res) => {
-    // TODO
+    pool.query(selectMovieDetailsText, [req.params.movieID])
+        .then(value => {
+            res.status(200).json(value.rows);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        })
 });
 
 // retrieve reviews and ratings for a movie
+const selectMovieReviewsText =
+    `
+    SELECT username, text, stars
+    FROM reviews WHERE reviews.movie_id = $1;
+    `;
+
 app.get("/movies/reviews/:movieID", (req, res) => {
-    // TODO
+    pool.query(selectMovieReviewsText, [req.params.movieID])
+        .then(value => {
+            res.status(200).json(value.rows);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        })
 });
 
 // retrieve the schedule for a movie
+const selectMovieScheduleText =
+    `
+    (
+    SELECT schedule_id, movie_id, theatre_id, date, time
+    FROM schedules
+    WHERE movie_id = $1
+    AND date >= CURRENT_DATE
+    )
+    EXCEPT
+    (
+    SELECT schedule_id, movie_id, theatre_id, date, time
+    FROM schedules
+    WHERE movie_id = $1
+    AND date = CURRENT_DATE
+    AND time <= LOCALTIME(0)
+    )
+    `;
+
 app.get("/movies/schedules/:movieID", (req, res) => {
-    // TODO
+    pool.query(selectMovieScheduleText, [req.params.movieID])
+        .then(value => {
+            res.status(200).json(value.rows);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        })
 });
 
 // check which seats are available for a scheduled movie
+const selectAvailableSeatsText =
+    `
+    SELECT all_seats.seat_id, all_seats.row, all_seats.number FROM
+    (
+	    SELECT seats.seat_id, seats.row, seats.number, seats.theatre_id FROM seats
+	    JOIN
+	    (
+		    SELECT schedules.theatre_id, schedules.schedule_id FROM schedules
+            WHERE schedules.schedule_id = $1
+	    ) theatre
+	    ON
+	    seats.theatre_id = theatre.theatre_id
+    ) all_seats
+    LEFT JOIN
+    (
+	    SELECT tickets.seat_id FROM tickets
+        WHERE tickets.schedule_id = $1
+    ) reserved_seats
+    ON
+    all_seats.seat_id = reserved_seats.seat_id
+    WHERE reserved_seats.seat_id IS NULL;
+    `;
+
 app.get("/movies/schedules/seats/:scheduleID", (req, res) => {
-    // TODO
+    pool.query(selectAvailableSeatsText, [req.params.scheduleID])
+        .then(value => {
+            res.status(200).json(value.rows);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // buy a ticket for a movie
+const insertTicketText =
+    `
+    INSERT INTO tickets(username, schedule_id, seat_id)
+    VALUES($1, $2, $3);
+    `;
+
 app.post("/tickets", (req, res) => {
-    // TODO
+    let body = req.body;  // specify 'content-type= application/json' when sending POST req!
+    if (!body.username || !body.schedule_id || !body.seat_id) {
+        res.status(400).send('Bad Request');
+    }
+
+    pool.query(insertTicketText, [body.username, body.schedule_id, body.seat_id])
+        .then(value => {
+            // normally resource link gets appended to resp.-body
+            res.status(200).send('OK');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // return a ticket for a movie
+// TODO: potential security risk?
+const deleteTicketText =
+    `
+    DELETE FROM tickets
+    WHERE ticket_id = $1;
+    `;
 app.delete("/tickets/:ticketID", (req, res) => {
-    // TODO
+    pool.query(deleteTicketText, [req.params.ticketID])
+        .then(value => {
+            res.status(204).send('No Content');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // rate and review a movie
+// TODO: check if user is has watched the movie
+const insertReviewText =
+    `
+    INSERT INTO reviews (movie_id, username, text, stars)
+    VALUES ($1, $2, $3, $4);
+    `;
 app.post("/movies/reviews", (req, res) => {
-    // TODO
+    let body = req.body;  // specify 'content-type= application/json' when sending POST req!
+    if (!body.movie_id || !body.username || !body.text || !body.stars) {
+        res.status(400).send('Bad Request');
+    }
+
+    pool.query(insertReviewText, [body.movieID, body.username, body.text, body.stars])
+        .then(value => {
+            // normally resource link gets appended to resp.-body
+            res.status(200).send('OK');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // ##################
@@ -108,3 +254,5 @@ app.delete("/schedules/:scheduleID", (req, res) => {
 let port = 3000;
 app.listen(port);
 console.log("Server running at: http://localhost:" + port);
+
+// TODO: implement HTTP-response status-code 418 I'm a teapot
