0b9053886e65a9118f1080118e8a82b417508105
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2023-01-17 14:42:42 +0100

    add login route using JWT

--- backend/rest_api/js/auth/login.js
+++ backend/rest_api/js/auth/login.js
@@ -1,1 +1,55 @@
-// TODO
+const cfg = require('../../config.json');
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const bodyParser = require('body-parser');
+const pool = require('../pool.js');
+
+const router = express.Router();
+router.use(bodyParser.json());
+
+// login for normal/admin users
+const selectUserText =
+    `
+    SELECT is_admin FROM users
+    WHERE username = $1 AND password = $2;
+    `;
+router.post('/login', (req, res) => {
+    let body = req.body;
+    if (body.username === undefined || body.password === undefined) {
+        res.status(400).send('Bad Request');
+        return;
+    }
+
+    pool.query(selectUserText, [body.username, body.password])
+        .then(value => {
+            let records = value.rows;
+
+            // no record found
+            if (records.length !== 1) {
+                res.status(401).send('Unauthorized');
+                return;
+            }
+
+            let token;
+            if (records[0].is_admin) {
+                token = jwt.sign({ admin: true }, cfg.auth.jwt_key,
+                    { expiresIn: cfg.auth.expiration, issuer: body.username });
+            } else {
+                token = jwt.sign({ admin: false }, cfg.auth.jwt_key,
+                    { expiresIn: cfg.auth.expiration, issuer: body.username });
+            }
+            res.status(200).json({
+                message: "Login successful",
+                user: body.username,
+                token: token
+            });
+        }
+        )
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        })
+
+});
+
+module.exports = router;
--- backend/rest_api/js/server.js
+++ backend/rest_api/js/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const login = require('./auth/login');
+const customer = require('./routes/customer');
+const management = require('./routes/management');
 
 const app = express();
-app.use(express.static('public'));
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.static('public'));
 
-const customer = require('./routes/customer');
+app.use(login);
 app.use(customer);
-
-const management = require('./routes/management');
 app.use(management);
 
-let port = 3000;
+const port = 3000;
 app.listen(port);
 console.log("Server running at: http://localhost:" + port);
